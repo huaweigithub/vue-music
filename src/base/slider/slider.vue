cebd2f2376cd46bcf88f4f8a6ce408c6631ecbff
huawei <huawei.lonny@gmail.com>
2019-04-09 22:19:29 +0800

    add slider

---
+++ src/base/slider/slider.vue
@@ -0,0 +1,95 @@
+<template>
+  <div class="slider" ref="slider">
+    <div class="slider-group" ref="sliderGroup">
+      <slot>
+
+      </slot>
+    </div>
+    <div class="dots"></div>
+  </div>
+</template>
+
+<script>
+import BScoll from 'better-scroll'
+import {addClass} from 'common/js/dom'
+export default {
+  props: {
+    loop : {
+      type: Boolean,
+      default: true
+    },
+    autoPlay: {
+      type: Boolean,
+      default: true
+    },
+    interval: {
+      type: Number,
+      default: 4000
+    }
+  },
+  mounted(){
+    setTimeout( ()=> {
+      this._setSliderWidth()
+      this._initSlider()
+    },20)
+  },
+  methods:{
+    _setSliderWidth(){
+      this.children = this.$refs.sliderGroup.children
+      let width = 0
+      let sliderWidth = this.$refs.slider.clientWidth
+      for(let i=0; i<this.children.length; i++){
+        let child = this.children[i]
+        addClass(child, 'slider-item')
+        child.style.width = sliderWidth + 'px'
+        width += sliderWidth
+      }
+      if(this.loop){
+        width += 2 * sliderWidth
+      }
+      this.$refs.sliderGroup.style.width = width + 'px'
+    },
+    _initSlider(){
+      this.slider = new BScoll(this.$refs.slider,{
+        scrollX: true,
+        scrollY: false,
+        momentum: false,
+        click: true,
+        snap: {
+          loop: this.loop,
+          threshold: 0.3,
+          speed: 400
+        },
+      })
+    }
+  }
+}
+</script>
+
+<style lang="stylus">
+.slider{
+  min-height : 1px;
+  overflow hidden
+  .slider-group{
+    position: relative;
+    overflow: hidden;
+    white-space :nowrap
+    .slider-item{
+      float:left;
+      overflow: hidden;
+      text-align : center
+      a{
+        display : block;
+        width : 100%;
+        overflow :hidden;
+        img{
+          display :block;
+          width :100%
+        }
+      }
+    }
+  }
+}
+</style>
+
+
--- src/components/recommend/recommend.vue
+++ src/components/recommend/recommend.vue
@@ -1,11 +1,27 @@
 <template>
-  <p>recommend...</p>
+  <div class="recommend-content">
+    <div v-if="recommends.length" class="slider-wrapper">
+      <slider>
+        <div v-for="item in recommends">
+          <a :href="item.linkUrl">
+            <img :src="item.picUrl">
+          </a>
+        </div>
+      </slider>
+    </div>
+  </div>
 </template>
 
 <script>
 import {getRecommend} from 'api/recommend'
 import {ERR_OK} from 'api/config'
+import Slider from '../../base/slider/slider'
 export default {
+  data(){
+    return{
+      recommends:[]
+    }
+  },
   created(){
     this._getRecommend()
   },
@@ -13,10 +29,14 @@ export default {
     _getRecommend() {
       getRecommend().then( (res) =>{
         if(res.code === ERR_OK) {
+          this.recommends = res.data.slider;
           console.log(res.data.slider)
         }
       } )
     }
   },
+  components:{
+    Slider
+  }
 }
 </script>
